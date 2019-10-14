045c80b4c689ff791f24d046c35eaabe0d35c9c6
Denis Shkabatur <denis6705@gmail.com>
2019-10-14 21:41:47 +0300

    add datetime field with cli-time data

--- src/artek_users_tracker/core.clj
+++ src/artek_users_tracker/core.clj
@@ -2,20 +2,12 @@
   (:require
     [clojure.string :as s]
     [clj-time.core :as t]
+    [artek-users-tracker.helpers :refer :all]
     )
   (:gen-class))
 
-(def regexp #"user:\s*(\S+)\s*computer:\s*(\S+)\s*date:\s*(\S+)\s*time:\s*(\S+)")
 
-(defn parse-users-time
-  [lines]
-  (let [parse-result (map #(re-find #"user:\s*(\S+)\s*computer:\s*(\S+)\s*date:\s*(\S+)\s*time:\s*(\S+)" %) lines)]
-    (map (fn [[ss user computer date time]]
-           {:user user
-            :computer computer
-            :date date
-            :time time})
-         parse-result)))
+
 
 
 
@@ -23,22 +15,12 @@
                         (slurp "login-time.txt")
                         (s/split-lines)
                         (parse-users-time)
-                        (set)
-                        (hash-map)
+                        ((fn [v] (filter #(not= nil (:user %)) v))) ;drop all non-parsed results
                         ))
 
 (def users-to-users-time
   (group-by :user users-login-time))
 
-;(def raw-text (slurp "login-time.txt"))
-;(def raw-lines (s/split-lines raw-text))
-;(def parsed-lines (map #(re-find #"user:\s*(\S+)\s*computer:\s*(\S+)\s*date:\s*(\S+)\s*time:\s*(\S+)" %) raw-lines))
-
-#_(def p-lines
-  (doseq [line raw-lines]
-    (let [[_ user computer date time] (re-find #"user:\s*(\S+)\s*computer:\s*(\S+)\s*date:\s*(\S+)\s*time:\s*(\S+)" line)]
-      (when (= user nil)
-        (pprint line)))))
 
 (defn -main
   "I don't do a whole lot ... yet."
---
+++ src/artek_users_tracker/helpers.clj
@@ -0,0 +1,43 @@
+(ns artek-users-tracker.helpers
+  (:require [clojure.string :as s]
+            [clj-time.core :as t]))
+
+
+(def regexp #"user:\s*(\S+)\s*computer:\s*(\S+)\s*date:\s*(\S+)\s*time:\s*(\S+)")
+
+(defn parse-int [s]
+  (Integer/parseInt (re-find #"\A-?\d+" s)))
+
+(defn parse-date
+  [date]
+  (into [] (->
+             date
+             (s/split #"\.")
+             (#(map parse-int %))
+             (reverse))))
+
+(defn parse-time
+  [time]
+  (into [] (->
+             time
+             (s/split #",")
+             (first)
+             (s/split #":")
+             (#(map parse-int %)))))
+
+
+(defn get-date-time
+  [date time]
+  (apply t/date-time (concat (parse-date date) (parse-time time)))
+  )
+
+(defn parse-users-time
+  [lines]
+  (let [parse-result (map #(re-find #"user:\s*(\S+)\s*computer:\s*(\S+)\s*date:\s*(\S+)\s*time:\s*(\S+)" %) lines)]
+    (map (fn [[_ user computer date time]]
+           (when (not= nil user) {:user user
+                                  :computer computer
+                                  :date date
+                                  :time time
+                                  :date-time (get-date-time date time)}))
+         parse-result)))
